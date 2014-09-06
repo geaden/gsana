1de6c18c77b0f261da20534ad41f19af8e6239fa
Gennady Denisov <denisovgena@gmail.com>
2014-09-06 12:29:48 +0400

    Move to OAuth.io authentication provider

--- app/src/main/java/com/geaden/android/gsana/app/LoginActivity.java
+++ app/src/main/java/com/geaden/android/gsana/app/LoginActivity.java
@@ -12,6 +12,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import io.oauth.OAuth;
+import io.oauth.OAuthCallback;
+import io.oauth.OAuthData;
 
 
 /**
@@ -37,28 +42,14 @@ public class LoginActivity extends ActionBarActivity {
     private final String WORKSPACE_ID = "498346170860";
     private final String ASSIGNEE = "";
 
+    private static final String OAUTH_GSANA_PUBLIC_KEY = "_3P90BSqgLGbIIXYz5olOtGUMCE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oauth);
         Intent intent = getIntent();
         Uri data = intent.getData();
-        // OAuth flow
-        if (data != null) {
-            Log.d(LOG_TAG, "Data: " + data);
-            // Data not null get parameters
-            String accessToken = data.getQueryParameter(ACCESS_TOKEN);
-            Log.d(LOG_TAG, "Access token from LoginActivity is " + accessToken);
-            // store auth token in default SharedPreferences
-            Log.v(LOG_TAG, "Saving access token...");
-            Editor e = getSharedPreferences(Constants.SHARED_PREF_KEY, Context.MODE_PRIVATE).edit();
-            e.putString(MainActivity.ACCESS_TOKEN_KEY, accessToken);
-            e.commit();
-            // Start Main activity
-            Intent mainIntent = new Intent(this, MainActivity.class);
-            startActivity(mainIntent);
-            return;
-        }
         // Show login button
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -67,10 +58,12 @@ public class LoginActivity extends ActionBarActivity {
         }
     }
 
+
+
     /**
     * A placeholder fragment containing a simple view.
     */
-    public static class LoginFragment extends Fragment {
+    public class LoginFragment extends Fragment implements OAuthCallback {
         private final String LOG_TAG = getClass().getSimpleName();
 
         public LoginFragment() { }
@@ -79,24 +72,42 @@ public class LoginActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_oauth, container, false);
+            final OAuth oAuth = new OAuth(getActivity());
+            oAuth.initialize(OAUTH_GSANA_PUBLIC_KEY);
             ImageButton asanaLogin = (ImageButton) rootView.findViewById(R.id.imageButton);
             asanaLogin.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Uri asanUri = Uri.parse(OAUTH_URL).buildUpon()
-                            .appendQueryParameter("client_id", CLIENT_ID)
-                            .appendQueryParameter("redirect_uri", CALLBACK_URL)
-                            .appendQueryParameter("response_type", RESPONSE_TYPE).build();
-                    Intent intent = new Intent(Intent.ACTION_VIEW);
-                    intent.setData(asanUri);
-                    if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
-                        startActivity(intent);
-                    } else {
-                        Log.d(LOG_TAG, "Couldn't call " + asanUri + ", no receiving apps installed!");
-                    }
+                    Log.v(LOG_TAG, "Authenticating...");
+                    oAuth.popup("asana", LoginFragment.this);
                 }
             });
             return rootView;
+        }
+
+        @Override
+        public void onFinished(OAuthData data) {
+            Log.v(LOG_TAG, "On finished...");
+            if (!data.status.equals("success")) {
+                Log.e(LOG_TAG, data.error);
+                Toast.makeText(getActivity(), data.error, Toast.LENGTH_SHORT);
+                return;
+            }
+            // OAuth flow
+            Log.d(LOG_TAG, "Access token: " + data.token);
+            // Data not null get parameters
+            String accessToken = data.token;
+            Log.d(LOG_TAG, "Access token from LoginActivity is " + accessToken);
+            // store auth token in default SharedPreferences
+            Log.v(LOG_TAG, "Saving access token...");
+            Editor e = getSharedPreferences(
+                    Constants.SHARED_PREF_KEY, Context.MODE_PRIVATE).edit();
+            e.putString(MainActivity.ACCESS_TOKEN_KEY, accessToken);
+            e.commit();
+            // Start Main activity
+            Intent mainIntent = new Intent(getActivity(), MainActivity.class);
+            startActivity(mainIntent);
+            return;
         }
     }
 }
